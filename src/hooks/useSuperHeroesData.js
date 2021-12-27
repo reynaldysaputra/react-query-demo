770919176d811d9a0a78512e21116159fb84c217
Renaldy <renalfrontend@gmail.com>
2021-12-28 00:36:09 +0700

    Demo 2

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 
 function fetchSuperHeroes(params) {
   return axios.get('http://localhost:4000/superheroes');
+}
+
+function addSuperHero(hero) {
+  return axios.post('http://localhost:4000/superheroes', hero);
 }
 
 export function useSuperHeroesData(props) {
@@ -12,9 +16,9 @@ export function useSuperHeroesData(props) {
     {
       cacheTime: 5000, // cache di set 5 detik, jadi jika query tidak aktif selama 5 detik dan ketika query aktif, maka dia akan refetch ulang
       staleTime: 0, // data basi, gunakan staleTime untuk mengatur data basi agar tidak merefetch ulang
-      refetchInterval: 2000, // fetch data setiap 2 detik sekali, ini berguna untuk mendapatkan data secara langsung
-      refetchIntervalInBackground: true, // fetch 2 detik sekali ketika memindahkan browser
-      enabled: false, // artinya jangan fetch data sebelum ada tindakan dari client, misalnya klik tombol
+      // refetchInterval: 2000, // fetch data setiap 2 detik sekali, ini berguna untuk mendapatkan data secara langsung
+      // refetchIntervalInBackground: true, // fetch 2 detik sekali ketika memindahkan browser
+      // enabled: false, // artinya jangan fetch data sebelum ada tindakan dari client, misalnya klik tombol
       onSuccess: (data) => console.log(`Perfom side effect after data fetching`, data),
       onError: (error) => console.log(`Perfom side effect after encountering error`, error),
       // select: data => {
@@ -23,4 +27,38 @@ export function useSuperHeroesData(props) {
       // }
     }
   );
+}
+
+export function useAddSuperHeroesData(){
+  const queryClient = useQueryClient();
+
+  return useMutation(addSuperHero, {
+    onSuccess: async ({ data }) => {
+      const previousHeroData = queryClient.getQueriesData('super-heroes');
+      
+      await queryClient.cancelQueries('super-heroes');
+
+      queryClient.setQueryData('super-heroes', (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [...oldQueryData.data, {
+            id: oldQueryData.data?.length + 1,
+            ...data
+          }]
+        }
+      })
+
+      return {
+        previousHeroData
+      }
+    },
+
+    onError: (error, _hero, context) => {
+      queryClient.setQueryData('super-heroes', context.previousHeroData);
+    },
+
+    onSettled: (data, error) => {
+      queryClient.invalidateQueries('super-heroes');
+    }
+  });
 }
--- src/screens/rqSuperHeroes.js
+++ src/screens/rqSuperHeroes.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+import { useAddSuperHeroesData, useSuperHeroesData } from "../hooks/useSuperHeroesData";
 
 function RqSuperHeroes(params) {
+  const [name, setName] = useState('');
+  const [alterEgo, setAlterEgo] = useState(''); 
+  const {mutate, error: mutateMessageError, isError: mutateError} = useAddSuperHeroesData();
   const { data: result, isLoading, isError, error, refetch, isFetching } = useSuperHeroesData();
+
+  const handleAddHero = (e) => {
+    e.preventDefault();
+    const hero = {name, alterEgo};
+    mutate(hero);
+  }
 
   if(isLoading) return <h2>Loading...</h2>;
 
@@ -12,6 +21,13 @@ function RqSuperHeroes(params) {
   return(
     <>
       <h2>RQ Super page</h2>
+      <form onSubmit={handleAddHero}>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+        <input type="text" value={alterEgo} onChange={(e) => setAlterEgo(e.target.value)}/>
+        {mutateError && mutateMessageError}
+        {console.log(mutateMessageError)}
+        <button type="submit">Add Hero</button>
+      </form>
       <button onClick={refetch}>Fetch heroes</button>
       {result?.data.map(hero => (
         <div key={hero.id}>
